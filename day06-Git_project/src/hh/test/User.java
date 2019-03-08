0d9e8e6dac2a8b5ebb7590f7dd3a1eb5da538998
huanghu <huanghu@it.cn>
2019-03-08 19:15:53 +0800

    提交工程

---
+++ day06-Git_project/src/hh/test/User.java
@@ -0,0 +1,10 @@
+package hh.test;
+
+public class User {
+    private static String name;
+    private static String sex;
+
+    public static void main(String[] args) {
+        System.out.println(name + "---" + sex);
+    }
+}
